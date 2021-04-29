eed6a1d6e9aa04a8498f26b5cf3d9935364eba85
NotSVASCO <semanavasco@gmail.com>
2021-04-29 11:44:45 +0200

    Update snipe.js

--- commands/moderation/snipe.js
+++ commands/moderation/snipe.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 module.exports = {
     name: 'snipe',
-    description: 'Envoie la dernière commande supprimée.',
+    description: 'Envoie le dernier message supprimé.',
     usage: '',
     guildOnly: true,
     cooldown: 5,
